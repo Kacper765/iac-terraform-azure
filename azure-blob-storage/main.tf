f6af0058bdd2ffa6914132a2c41ab06e14c7b9f6
Kacper765 <75902591+Kacper765@users.noreply.github.com>
2022-05-22 11:37:42 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,1 +1,30 @@
+resource "azurerm_resource_group" "group" {
+  name     = var.example-resources
+  location = var.North Europe
+}
 
+resource "azurerm_storage_account" "account" {
+  name                     = var.storageaccountname
+  resource_group_name      = azurerm_resource_group.group.name
+  location                 = azurerm_resource_group.group.location
+  account_tier             = var.Standard
+  account_replication_type = var.GRS
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.content
+  storage_account_name  = azurerm_storage_account.account.name
+  container_access_type = var.private
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name                   = var.my-awesome-content.zip
+  storage_account_name   = azurerm_storage_account.account.name
+  storage_container_name = azurerm_storage_container.container.name
+  type                   = var.Block
+  source                 = var.some-local-file.zip
+}
