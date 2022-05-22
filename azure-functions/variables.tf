906b618bc31204431fa277a9affd6bfe3d631523
Kacper765 <75902591+Kacper765@users.noreply.github.com>
2022-05-22 10:50:57 +0200

    Update variables.tf

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,1 +1,24 @@
+variable "resource_group" {
+  type = string
+  default = "test"
+}
 
+variable "storage_account" {
+  type = string
+  default = "test"
+}
+
+variable "service_plan" {
+  type = string
+  default = "test"
+}
+
+variable "linux_funtion_app" {
+  type = string
+  default = "test"
+}
+
+variable "function_app_function" {
+  type = string
+  default = "test"
+}
