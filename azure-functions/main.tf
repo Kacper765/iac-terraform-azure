e7c07567c64b910f3f34b42fb9a67fcde6b56167
Kacper765 <75902591+Kacper765@users.noreply.github.com>
2022-05-22 10:47:32 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,43 +1,43 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "North Europe"
+resource "azurerm_resource_group" "group" {
+  name     = var."example-resources"
+  location = var."North Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+resource "azurerm_storage_account" "account" {
+  name                     = var."storageaccountname"
+  resource_group_name      = azurerm_resource_group.group.name
+  location                 = azurerm_resource_group.group.location
+  account_tier             = var."Standard"
+  account_replication_type = var."GRS"
 
   tags = {
     environment = "staging"
   }
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  os_type             = "Linux"
-  sku_name            = "FREE"
+resource "azurerm_service_plan" "plan" {
+  name                = var."example"
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
+  os_type             = var."Linux"
+  sku_name            = var."FREE"
 }
 
-resource "azurerm_linux_function_app" "example" {
-  name                = "example-linux-function-app"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_linux_function_app" "app" {
+  name                = var."example-linux-function-app"
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
 
-  storage_account_name = azurerm_storage_account.example.name
-  service_plan_id      = azurerm_service_plan.example.id
+  storage_account_name = azurerm_storage_account.account.name
+  service_plan_id      = azurerm_service_plan.plan.id
 
   site_config {}
 }
 
-resource "azurerm_function_app_function" "example" {
-  name            = "example-function-app-function"
-  function_app_id = azurerm_linux_function_app.example.id
-  language        = "Python"
+resource "azurerm_function_app_function" "function" {
+  name            = var."example-function-app-function"
+  function_app_id = azurerm_linux_function_app.app.id
+  language        = var."Python"
   test_data = jsonencode({
     "name" = "Azure"
   })
