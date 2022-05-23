829433bdbf235160ab22a755485ad3227dfaa2bd
Kacper765 <75902591+Kacper765@users.noreply.github.com>
2022-05-23 18:57:18 +0200

    Update variables.tf

--- azure-linux-web-app/variables.tf
+++ azure-linux-web-app/variables.tf
@@ -1,1 +1,14 @@
+variable "resource_group" {
+  type = string
+  default = "test"
+}
 
+variable "service_plan" {
+  type = string
+  default = "test"
+}
+
+variable "linux_web_app" {
+  type = string
+  default = "test"
+}
