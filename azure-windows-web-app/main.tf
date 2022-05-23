ce3bc76aaf3482fb56586ace2b0901e0b9472ed1
Kacper765 <75902591+Kacper765@users.noreply.github.com>
2022-05-23 18:52:20 +0200

    Update main.tf

--- azure-windows-web-app/main.tf
+++ azure-windows-web-app/main.tf
@@ -1,1 +1,25 @@
+# azurerm_app_service
+# Manages an App Service (within an App Service Plan).
+# This resource has been deprecated in version 3.0 of the AzureRM provider and will be removed in version 4.0. 
+# Please use azurerm_linux_web_app and azurerm_windows_web_app resources instead.
 
+resource "azurerm_resource_group" "group" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_windows_web_app" "app" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_service_plan.plan.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {}
+}
